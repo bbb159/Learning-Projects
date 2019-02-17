5207fb359d93cdf06c78ea0ba199555e3350e6d4
brunop <brunop@kyros.com.br>
2019-02-17 14:52:39 -0300

    Signing transactions

--- NodeJS - Blockchain/src/blockchain.js
+++ NodeJS - Blockchain/src/blockchain.js
@@ -57,6 +57,13 @@ class Block {
         console.log('Block mined: ', this.hash);
     }
 
+    hasValidTransactions() {
+        for(const tx of this.transactions){
+            if(!tx.isValid()) return false;
+        }
+        return true;
+    }
+
 }
 
 class Blockchain {
@@ -76,20 +83,27 @@ class Blockchain {
     }
 
     minePendingTransactions(miningRewardAddress) {
-        let block = new Block(Date.now(), this.pendingTransactions);
-        block.previousHash = this.getLatestBlock().hash;
+
+        const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
+        this.pendingTransactions.push(rewardTx);
+
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
         block.mineBlock(this.difficulty);
 
         console.log('Block successfully mined');
 
         this.chain.push(block);
 
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAddress, this.miningReward)
-        ];
+        this.pendingTransactions = [];
     }
 
-    createTransaction(transaction) {
+    addTransaction(transaction) {
+        if(!transaction.fromAddress || !transaction.toAddress)
+            throw new Error('Transaction must include from and to address');
+
+        if(!transaction.isValid())
+            throw new Error('Cannot add invalid transaction to chain');
+        
         this.pendingTransactions.push(transaction);
     }
 
@@ -110,6 +124,9 @@ class Blockchain {
         for (let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
+
+            if (!currentBlock.hasValidTransactions())
+                return false;
 
             if (currentBlock.hash !== currentBlock.calculateHash())
                 return false;
--- NodeJS - Blockchain/src/main.js
+++ NodeJS - Blockchain/src/main.js
@@ -1,20 +1,24 @@
 const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+//Public key:  04b78045547ccb7b3ee829ab6d572d78e5f03deba00c9539c98000bcc7a4423b8815f237f42e6d8e669c5961b76ce7219db96470fcc2eb4d82829c72721396c308
+const myKey = ec.keyFromPrivate('88001a0689656587ba470d44b6f63e0ffc8d92d6dcf7631a6aa95cdcb0c966f6');
+const myWalletAddress = myKey.getPublic('hex');
 
 let paivaCoin = new Blockchain();
-paivaCoin.createTransaction(new Transaction('address1', 'address2', 100));
-paivaCoin.createTransaction(new Transaction('address2', 'address1', 50));
+
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+paivaCoin.addTransaction(tx1);
 
 console.log('\n Starting the miner \n');
 
-paivaCoin.minePendingTransactions('bruno-address');
+paivaCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of Bruno is ', paivaCoin.getBalanceOfAddress('bruno-address'));
-
-console.log('\n Starting the miner again... \n');
-
-paivaCoin.minePendingTransactions('bruno-address');
-
-console.log('\nBalance of Bruno is ', paivaCoin.getBalanceOfAddress('bruno-address'));
+console.log('\nBalance of Bruno is ', paivaCoin.getBalanceOfAddress(myWalletAddress));
 
 console.log('\nBLOCK CHAIN:\n');
 console.log(JSON.stringify(paivaCoin, null, 4));
+
+
